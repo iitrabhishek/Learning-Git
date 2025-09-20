cfac14f87837d857a0583ff774ad94bc23d58ac6
iitrabhishek <4abhisheksaxena@gmail.com>
2025-09-20 21:02:53 +0530

    add courses to homepage

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ st.title('Campusx')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.write('Hello world')
+    st.image("img.png")
 
 with col2:
     st.write("""
-    Maruti Ertiga price for the base model starts at Rs. 10.40 Lakh and the top model price goes upto Rs. 15.72 Lakh 
-    (on-road Bareilly). Ertiga price for 9 variants is listed below.
+    Hi, my name is Nitish. I am an educational content creator on YouTube with 150K+ Subscribers in the domain of 
+    Data Science. I have been in the tech industry for the past 10 years and taught more than 50,000 students offline. 
+    I am passionate about data and take pride in creating content that simplifies complex topics.
     """)
+
+st.header('Coureses Offered')
+st.subheader('Data Science')
+st.subheader('Data Analysis')
+st.subheader('Data Engineering')
